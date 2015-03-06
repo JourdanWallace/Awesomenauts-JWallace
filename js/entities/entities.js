62dc6d054091902580a6c12abf8065c9093bffa4
Jourdan Wallace <jaywallace713@gmail.com>
2015-03-06 09:19:49 -0800

    Finished milestone 1, YAY!

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,15 @@
-// TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function (x, y, settings){
+        this._super(me.Entity, 'init'[x, y, {
+            image: "player",
+            width: 64,
+            height: 64,
+            spritewidth: "64",
+            spriteheight: "64",
+        }]);
+    },
+    
+    update: function (){
+        
+    }
+});
