13e6d6a00695861c14a20995f176217f4a7985d2
Jourdan Wallace <jaywallace713@gmail.com>
2015-03-12 09:20:26 -0700

    Finished milestone 3 with no errors.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -62,13 +62,61 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        
+        console.log("init");
         this.type = "PlayerBaseEntity";
         
+        this.renderable.addAnimation("idle", [0]);
+        this.renderable.setCurrentAnimation("idle");
     },
     
-    update:function(){
-        
-    }          
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);         
     
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+
+});
+
+game.EnemyBaseEntity = me.Entity.extend({
+    init : function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "tower",
+                width: 100,
+                height: 100,
+                spritewidth: "100",
+                spriteheight: "100",
+                getShape: function(){
+                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                }               
+        }]);  
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "EnemyBaseEntity";
+    },
+    
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);         
+    
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+
 });
