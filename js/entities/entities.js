e01b97f2740dde6843ed25f1ea76334fb90bcde7
Jourdan Wallace <jaywallace713@gmail.com>
2015-03-25 09:20:19 -0700

    I don't have an error but  there's something wrong

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -120,8 +120,7 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        console.log("init");
-        this.type = "PlayerBaseEntity";
+        this.type = "PlayerBase";
 
         this.renderable.addAnimation("idle", [0]);
         this.renderable.addAnimation("broken", [1]);
@@ -137,6 +136,10 @@ game.PlayerBaseEntity = me.Entity.extend({
 
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    loseHealth: function(damage){
+        this.health = this.health = damage;
     },
     
     onCollision: function() {
@@ -204,7 +207,13 @@ game.EnemyCreep = me.Entity.extend({
         }]);
         this.health = 10;
         this.alwaysUpdate = true;
-        
+        //this.attacking lets us know if the enemy is currently attacking
+        this.attacking = false;
+        //keeps track of when our creep last attacked anything
+        this.lastAttacking = new Date().getTime();
+        //keeps track of the last time our ceep hit anything
+        this.lastHit = new Date().getTime();
+        this.now = new Date().getTime();
         this.body.setVelocity(3, 20);
         
         this.type = "EnemyCreep";
@@ -215,19 +224,39 @@ game.EnemyCreep = me.Entity.extend({
     },
     
     update: function(delta) {
-        
+        this.now = new Date().getTime();
         
         this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
         
         
         this.body.update(delta);
         
 
         
-        this._super(me.Entity, "update", [delta]);
-        
+        this._super(me.Entity, "update", [delta]);       
         return true;
-    }  
+    },
+    
+    collideHandler: function(response){
+        if(response.b.type==='PlayerBase'){
+            this.attacking=true;
+            //this.lastAttacking=this.now;
+            this.body.vel.x = 0;
+            //keeps moving the creep to the right to maintain its position 
+            this.pos.x = this.pos.x + 1; 
+            //checls that it has been one second since the creep hit a base
+            if((this.now-this.lastHit >= 1000)){
+                //updates the last hit timer
+                this.lastHit = this.now;
+                //makes the player base call its lose health function annd passes it
+                //a damage 1
+                response.b.loseHealth(1);
+            }
+        }
+    }
+    
 });
 
 game.GameManager = Object.extend({
