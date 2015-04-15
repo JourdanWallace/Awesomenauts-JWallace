feba84383c381ef7c60c036418aee811a20a010d
Jourdan Wallace <jaywallace713@gmail.com>
2015-04-15 09:18:14 -0700

    On video 49!

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -186,7 +186,7 @@ game.PlayerEntity = me.Entity.extend({
     checkAttack: function(xdif, ydif){
         if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
                    && (Math.abs(ydif) <=40) && 
-                   (((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right")) 
+                   (((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing === "right")) 
                    ){
                 this.lastHit = this.now;
                 //if the creeps health is less than our attack, execute code in if statement
